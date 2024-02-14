4c1a3b95fb674b869d8424d62bdb535d015c02f8
Julian Gonggrijp <j.gonggrijp@uu.nl>
2024-02-14 14:39:17 +0100

    Add tests for fastTimeout

---
+++ src/fastTimeout.test.js
@@ -0,0 +1,36 @@
+import assert from 'assert';
+import { after, now } from 'underscore';
+import { optIn, fastTimeout } from './fastTimeout.js';
+
+describe('fastTimeout', function() {
+    before(optIn);
+
+    it('can schedule a task', function(done) {
+        fastTimeout(done);
+    });
+
+    it('is faster than setTimeout', function(done) {
+        var fast, normal, finish;
+
+        function downcounter(method) {
+            var remaining = 10;
+            function countDown() {
+                if (--remaining) return method(countDown);
+                countDown.endTime = now();
+                finish();
+            }
+            return countDown;
+        }
+
+        fast = downcounter(fastTimeout);
+        normal = downcounter(setTimeout);
+        finish = after(2, function() {
+            assert(fast.endTime < normal.endTime);
+            done();
+        });
+
+        // We even give setTimeout a head start!
+        setTimeout(normal);
+        fastTimeout(fast);
+    });
+});
