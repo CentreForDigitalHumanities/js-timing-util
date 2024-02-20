e581041c5d817c1a9d09af1e9a067b7b6a9e7c92
Julian Gonggrijp <j.gonggrijp@uu.nl>
2024-02-20 13:00:31 +0100

    Eliminate any needs for transpilation

--- src/fastTimeout.js
+++ src/fastTimeout.js
@@ -1,3 +1,5 @@
+import { restArguments } from 'underscore';
+
 // The code in this module was inspired on the following blog post:
 // https://dbaron.org/log/20100309-faster-timeouts
 // In this module, we use a different name, enable passing arguments and use ES6
@@ -10,18 +12,18 @@ function handleMessage(event) {
     if (event.source === self && event.data === messageName) {
         event.stopPropagation();
         if (self[timeouts].length > 0) {
-            var { fn, args } = self[timeouts].shift();
-            fn(...args);
+            var job = self[timeouts].shift();
+            job.fn.apply(null, job.args);
         }
     }
 }
 
 // Like setTimeout, but takes no time argument (always zero) and is faster in
 // real browsers. setTimeout is throttled to 2-10 ms intervals.
-function fastTimeout(fn, ...args) {
-    self[timeouts].push({ fn, args });
+var fastTimeout = restArguments(function(fn, args) {
+    self[timeouts].push({fn: fn, args: args});
     self.postMessage(messageName, '*');
-}
+});
 
 function enable() {
     if (timeouts in self) return fastTimeout;
